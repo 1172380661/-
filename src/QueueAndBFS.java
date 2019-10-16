b982c93ea8319160ebcf61958838ac4145fef85f
wsw <1172380661@qq.com>
2019-10-16 16:01:28 +0800

    队列和BFS相关

--- src/QueueAndBFS.java
+++ src/QueueAndBFS.java
@@ -33,7 +33,7 @@ public class QueueAndBFS {
 
     private void BFS(boolean[][] visited, char[][] grid, int i, int j) {
         //递归
-/*        if (i<0||i>grid.length||j<0||j>grid[0].length||visited[i][j]||grid[i][j]=='0'){
+/*        if (i<0||i>=grid.length||j<0||j>=grid[0].length||visited[i][j]||grid[i][j]=='0'){
             return;
         }
         visited[i][j] = true;
@@ -66,7 +66,6 @@ public class QueueAndBFS {
         if(deadendsSet.contains("0000")){
             return -1;
         }
-        HashSet<String> visited = new HashSet<>();
         queue.offer("0000");
         int count = 0;
         while (!queue.isEmpty()){
@@ -78,8 +77,9 @@ public class QueueAndBFS {
                 }
                 List<String> nexts = getNexts(poll);
                 for (String string:nexts){
-                    if (!deadendsSet.contains(string)&&visited.add(string)){
+                    if (!deadendsSet.contains(string)){
                         queue.offer(string);
+                        deadendsSet.add(string);
                     }
                 }
             }
@@ -91,14 +91,62 @@ public class QueueAndBFS {
     private List<String> getNexts(String str){
         List<String> list = new ArrayList<>();
         for (int i=0;i<4;i++){
-            StringBuffer sb1 = new StringBuffer(str);
+            StringBuilder sb1 = new StringBuilder(str);
             sb1.setCharAt(i,sb1.charAt(i)=='9'?'0': (char) (sb1.charAt(i) + 1));
             list.add(sb1.toString());
-            StringBuffer sb2 = new StringBuffer(str);
+            StringBuilder sb2 = new StringBuilder(str);
             sb2.setCharAt(i,sb2.charAt(i)=='0'?'9': (char) (sb2.charAt(i) - 1));
             list.add(sb2.toString());
         }
         return list;
     }
+
+    /**
+     * 给定正整数 n，找到若干个完全平方数（比如 1, 4, 9, 16, ...）使得它们的和等于 n。
+     * 你需要让组成和的完全平方数的个数最少。
+     * @param n 给定数
+     * @return 最少由几个完全平方数组成
+     */
+    public int numSquares(int n) {
+/*        //动态规划思路
+        int[] dp = new int[n + 1];
+        Arrays.fill(dp, Integer.MAX_VALUE);
+        dp[0] = 0;
+        for (int i=1;i<dp.length;i++){
+            for (int j=(int)Math.sqrt(i);j>0;j--){
+                dp[i] = Math.min(dp[i], dp[i-j*j]+1);
+            }
+        }
+        return dp[n];*/
+        //BFS思路
+        int num = (int)Math.sqrt(n);
+        Queue<Integer> queue = new LinkedList<>();
+        boolean[] visit = new boolean[n + 1];
+        while (num>0){
+            queue.offer(num*num);
+            visit[num*num] = true;
+            num--;
+        }
+        int size = 0;
+        outer:
+        while (!queue.isEmpty()){
+            size++;
+            int len = queue.size();
+            for (int i=0;i<len;i++){
+                Integer poll = queue.poll();
+                if (poll == n){
+                    break outer;
+                }
+                for (int j=(int)Math.sqrt(n-poll);j>0;j--){
+                    if (!visit[poll+j*j]){
+                        queue.offer(poll+j*j);
+                        visit[poll+j*j] = true;
+                    }
+                }
+            }
+        }
+        return size;
+    }
+
 
 }
